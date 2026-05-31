e3d3cc29c92914f73d2d92b9d7163a687e4ab8c0
Khavin Shankar <khavinshankar@gmail.com>
2026-05-31 16:52:27 +0530

    fixed skeleton loading

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -8,7 +8,7 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-muted/50", className)}
+      className={cn("animate-pulse rounded-md bg-gray-200", className)}
       {...props}
     />
   );
